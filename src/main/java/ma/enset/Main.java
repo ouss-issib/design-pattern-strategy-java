76406d762a60897acdb6d56a082356280d9ded90
oussama bissi <oussamaissib@gmail.com>
2025-11-04 12:20:25 +0100

    feat : verify if strategy was in cashed local..

--- src/main/java/ma/enset/Main.java
+++ src/main/java/ma/enset/Main.java
@@ -1,5 +1,7 @@
 package ma.enset;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -9,12 +11,20 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Context context = new Context();
         Scanner scanner = new Scanner(System.in);
+        Map<String,Strategy> strategyMap = new HashMap<>();
+        Strategy strategy;
         while (true) {
             System.out.println("Quelle Stratégie ? ");
             String strategyClassName =  scanner.next();
-            Strategy strategy = (Strategy) Class.forName("ma.enset."+strategyClassName).getConstructor().newInstance();
+            strategy = strategyMap.get(strategyClassName);
+            if(strategy == null) {
+                System.out.println("Création d'un nouvel objet de strategyIMP");
+                strategy = (Strategy) Class.forName("ma.enset."+strategyClassName).getConstructor().newInstance();
+                strategyMap.put(strategyClassName,strategy);
+            }
             context.setStrategy(strategy);
             context.effectuerOperation();
+
         }
     }
 }
